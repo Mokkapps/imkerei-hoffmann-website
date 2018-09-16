a706d94b2b65c263187738747994e9aae4439616
Michael Hoffmann <kontakt@mokkapps.de>
2018-09-16 21:12:08 +0200

    Correctly configure dotenv

--- src/components/GoogleMap.jsx
+++ src/components/GoogleMap.jsx
@@ -12,7 +12,7 @@ export default () => {
     <MapView
       frameBorder="0"
       src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJz7lFCAhFdUcR4GJ5RrF7uKY&key=${
-        process.env.GOOGLE_MAPS_API_KEY
+        process.env.GATSBY_GOOGLE_MAPS_API_KEY
       }`}
       allowFullScreen
     />
