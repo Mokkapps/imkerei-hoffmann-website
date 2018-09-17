536c445504d03eba65ccbaae0cb113544a66b9d4
Michael Hoffmann <kontakt@mokkapps.de>
2018-09-17 08:44:32 +0200

    Improved SEO information

--- src/layout/index.js
+++ src/layout/index.js
@@ -27,8 +27,15 @@ const Layout = ({ children }) => (
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            {
+              name: 'description',
+              content:
+                'Alle wichtigen Informationen über die Privatimkerei-Hoffmann',
+            },
+            {
+              name: 'keywords',
+              content: 'imkerei, privatimkerei, honig, bienenwachstücher',
+            },
           ]}
         >
           <html lang="de" />
